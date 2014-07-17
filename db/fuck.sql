96961a184582dc2d1f07c1f1bba6a6fc1ae5dd37
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2014-07-17 01:17:51 -0500

    bds
    fuck es la bd de la app new_berlin. cagada es de caCAS

--- db/fuck.sql
+++ db/fuck.sql
@@ -23,16 +23,17 @@ DROP TABLE IF EXISTS `t001_user_cat`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t001_user_cat` (
-  `id_user` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
   `tx_username` varchar(255) NOT NULL,
   `tx_email` varchar(255) DEFAULT NULL,
   `bl_enabled` smallint(6) DEFAULT NULL,
   `bl_account_expired` smallint(6) DEFAULT NULL,
   `bl_account_locked` smallint(6) DEFAULT NULL,
   `bl_password_expired` smallint(6) DEFAULT NULL,
+  `version` bigint(20) NOT NULL,
   PRIMARY KEY (`id_user`),
   UNIQUE KEY `username_UNIQUE` (`tx_username`)
-) ENGINE=InnoDB DEFAULT CHARSET=big5;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=big5;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +42,7 @@ CREATE TABLE `t001_user_cat` (
 
 LOCK TABLES `t001_user_cat` WRITE;
 /*!40000 ALTER TABLE `t001_user_cat` DISABLE KEYS */;
+INSERT INTO `t001_user_cat` VALUES (1,'codeisdesign','mierda@cagada.com',1,1,1,0,0),(2,'caca','dqwdwq',0,0,0,0,0);
 /*!40000 ALTER TABLE `t001_user_cat` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,11 +54,12 @@ DROP TABLE IF EXISTS `t002_role_cat`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t002_role_cat` (
-  `id_role` int(11) NOT NULL,
-  `tx_authority` varchar(255) NOT NULL COMMENT 'menuda mierda',
+  `id_role` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `tx_authority` varchar(255) NOT NULL,
   PRIMARY KEY (`id_role`),
-  UNIQUE KEY `tx_authority_UNIQUE` (`tx_authority`)
-) ENGINE=InnoDB DEFAULT CHARSET=big5;
+  UNIQUE KEY `UK_a46wvsbwqc499jl6ygq0cy8vd` (`tx_authority`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=big5;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +68,32 @@ CREATE TABLE `t002_role_cat` (
 
 LOCK TABLES `t002_role_cat` WRITE;
 /*!40000 ALTER TABLE `t002_role_cat` DISABLE KEYS */;
+INSERT INTO `t002_role_cat` VALUES (1,0,'ROLE_USER'),(2,0,'ROLE_NOMAMESSS'),(3,0,'fuck authority'),(4,0,'dwdqwwqqqqq');
 /*!40000 ALTER TABLE `t002_role_cat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t003_user_role_cat`
+--
+
+DROP TABLE IF EXISTS `t003_user_role_cat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t003_user_role_cat` (
+  `role_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`role_id`,`user_id`),
+  CONSTRAINT `FK_ksrldypwamd2eyolbqxulbbtl` FOREIGN KEY (`role_id`) REFERENCES `t002_role_cat` (`id_role`)
+) ENGINE=InnoDB DEFAULT CHARSET=big5;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t003_user_role_cat`
+--
+
+LOCK TABLES `t003_user_role_cat` WRITE;
+/*!40000 ALTER TABLE `t003_user_role_cat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t003_user_role_cat` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -100,4 +128,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-07-12 15:11:21
+-- Dump completed on 2014-07-17  1:16:59
